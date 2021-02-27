36a7d6f021f9cf896d1442e705438356d596a3a9
Margaret <popamargaret18@stud.ase.ro>
2021-02-27 20:07:46 +0200

    Final commit

---
+++ Seminar1/src/clase/Lion.java
@@ -0,0 +1,11 @@
+package clase;
+
+public class Lion extends Animal {
+
+	public Lion(String nume) {
+		super(nume);
+		// TODO Auto-generated constructor stub
+	
+	}
+
+}
--- Seminar1/src/seminar1/Main.java
+++ Seminar1/src/seminar1/Main.java
@@ -1,10 +1,26 @@
 package seminar1;
 
+import clase.Lion;
+import clase.WildCat;
+import clase.Zoo;
+
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+       Zoo zoo= new Zoo();
+       Lion lion1= new Lion("Leo");
+       Lion lion2= new Lion("Leona");
+       zoo.addAnimal(lion1);
+       zoo.addAnimal(lion2);
+       zoo.feedAllAnimals();
+       
+       WildCat cat = new WildCat("pisica");
+       WildCat cat1=new WildCat("pisica2","mancare");
+       zoo.addAnimal(cat);
+       zoo.addAnimal(cat1);
+       zoo.feedAllAnimals();
+       
 	}
 
 }
