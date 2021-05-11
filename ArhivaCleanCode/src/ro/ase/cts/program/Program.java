250e9befdb8e396a06508d0d027f419e42a655f1
margaretpopa <popamargaret18@stud.ase.ro>
2021-05-11 19:16:31 +0300

    ArhivaProba

---
+++ ArhivaCleanCode/src/ro/ase/cts/program/Program.java
@@ -0,0 +1,29 @@
+package ro.ase.cts.program;
+
+import java.io.FileNotFoundException;
+import java.util.List;
+
+import ro.ase.cts.clase.Angajat;
+import ro.ase.cts.clase.Aplicant;
+import ro.ase.cts.readere.*;
+
+public class Program {
+	
+	public static List<Aplicant> citireAplicanti(ReaderAplicant readerAplicant) throws FileNotFoundException
+	{
+		return readerAplicant.readAplicanti();
+	}
+
+	public static void main(String[] args) {
+		List<Aplicant> listaAplicanti;
+		try {
+			listaAplicanti = citireAplicanti(new ReaderStudent("studenti.txt"));
+			for(Aplicant aplicant:listaAplicanti)
+				System.out.println(aplicant.toString());
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+}
---
+++ ArhivaCleanCode/src/ro/ase/cts/readere/ReaderAplicant.java
@@ -0,0 +1,22 @@
+package ro.ase.cts.readere;
+
+import java.io.FileNotFoundException;
+import java.util.List;
+
+import ro.ase.cts.clase.Aplicant;
+
+public abstract class ReaderAplicant {
+
+	protected String numeFisier;
+	
+	
+	public abstract List<Aplicant> readAplicanti() throws FileNotFoundException;
+
+
+	public ReaderAplicant(String numeFisier) {
+		super();
+		this.numeFisier = numeFisier;
+	}
+	
+	
+}
